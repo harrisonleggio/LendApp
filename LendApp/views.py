37679e71ec67ad074c27ffeb66060bfc0bfbf079
harrisonleggio <harrisonleggio@my.uri.edu>
2017-10-13 23:40:59 -0400

    generating card token + creating charge on it

--- LendApp/views.py
+++ LendApp/views.py
@@ -1,6 +1,7 @@
 from LendApp import *
 from flask import request
-from LendApp.stripe_modules import card
+from LendApp.stripe_modules import card, charge
+import stripe
 
 @app.route('/')
 def index():
@@ -17,6 +18,7 @@ def process_transaction():
 
     return output_vals
 
+
 @app.route('/card_token')
 def generate_card_token():
     number = request.args.get('number')
@@ -24,10 +26,21 @@ def generate_card_token():
     exp_year = request.args.get('exp_year')
     cvc = request.args.get('cvc')
 
+    amount = request.args.get('amount')
+    description = request.args.get('description')
+    receipt_email = request.args.get('receipt_email')
+
     generate = card.card_token(number, exp_month, exp_year, cvc)
-    c_token = generate.card_token()
+    card_token = generate.generate_card_token()
 
-    return c_token
+    print stripe.Token.retrieve(card_token)
+    return generate_charge(card_token, amount, description, receipt_email)
 
 
+def generate_charge(token, amount, description, receipt_email):
+    generate = charge.charge_token(amount, description, receipt_email, token)
+    ch_token = generate.generate_charge_token()
+
+    print ch_token
+    return ch_token['id']
 
