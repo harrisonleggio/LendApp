e12d3388788fb0bea6bbb60f5e0cd642091d60ce
harrisonleggio <harrisonleggio@my.uri.edu>
2017-10-15 20:08:56 -0400

    testing kivy

--- Kivy_App.py
+++ Kivy_App.py
@@ -1,16 +1,24 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.properties import ObjectProperty
-
+from kivy.uix.boxlayout import BoxLayout
 
 class Request(Screen):
     pass
 
-
 class Send(Screen):
-    pass
+    amount_input = ObjectProperty()
+    receiver_input = ObjectProperty()
+    interest_input = ObjectProperty()
 
+    def process_amount(self):
+        amount = self.amount_input.text
+        receiver = self.receiver_input.text
+        interest = self.interest_input.text
 
+        print amount
+        print receiver
+        print interest
 
 class Manager(ScreenManager):
 
